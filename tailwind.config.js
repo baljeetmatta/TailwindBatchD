5c81106ae13e7ee1dd7302c0c1391a7ec4203939
Baljeet Matta <baljeetmatta@Baljeets-MacBook-Air.local>
2025-01-24 13:43:27 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+container:{
+  padding:'3rem',
+  center:true
+},    
+    extend: {},
+  },
+  plugins: [],
+}
+
